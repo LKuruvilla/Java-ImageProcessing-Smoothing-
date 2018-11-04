041b8e712e4a761951323a1cf0160511cc155d6d
LovinK <lovinkuruvilla@gmail.com>
2018-11-04 12:57:23 -0600

    Single threaded Program
    Added classes and objects to successfully smooth the image

--- src/ImageProcessingProject.java
+++ src/ImageProcessingProject.java
@@ -7,158 +7,188 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
 
-
-
-
-
-public class ImageProcessingProject {
+class ImageProcess {
+    public BufferedImage current, redrawn;
+    public int[] largeSquare;
+    public int[] smallSquare;
+    public int width, height;
+    public int newpixelvalue;
     
-    public static void main (String[] args){
-        File f =null;    
-        BufferedImage img = null;
+    public ImageProcess(BufferedImage input){
+        current = input;
+        width = input.getWidth();
+        height = input.getHeight();
+        redrawn = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
+        smallSquare = new int[4];
+        largeSquare = new int[9];
+        
+    }
+    public void saveFile(){
         try{
-            img = ImageIO.read(new File("old.bmp"));
-        }catch (Exception e)
-        {
-            e.getLocalizedMessage();
-        }
-        int pixels =0;
-        int[]p = new int[9];
-        int h = img.getHeight();
-        int w= img.getWidth();
-        BufferedImage nu = new BufferedImage(img.getWidth(),img.getHeight(),BufferedImage.TYPE_INT_RGB);
-        int newpixelvalue =0, total =0;
-        int[]x = new int[4];
-        
-        for(int i =0; i<w; i++)
+         
+            File f = new File("new.bmp");
             
-        {
-            
-            for(int j=0;j<h ;j++ )
-            {   
-                //top left corner
-                if(i==0 &&j==0)
-                { 
-                    x[0] = img.getRGB(i, j);
-                    x[1] = img.getRGB(i, j+1);
-                    x[2] = img.getRGB(i+1, j+1);
-                    x[3] = img.getRGB(i+1, j);
-                    newpixelvalue = (x[0]+x[1]+x[2]+x[3])/4;
-                    nu.setRGB(i, j, newpixelvalue);
-                    
-                }
-                //top right corner
-                else if(i==w-1&&j==0)
-                {
-                    x[0] = img.getRGB(i, j);
-                    x[1] = img.getRGB(i-1, j);
-                    x[2] = img.getRGB(i-1, j+1);
-                    x[3] = img.getRGB(i, j+1);
-                    newpixelvalue = (x[0]+x[1]+x[2]+x[3])/4;
-                    nu.setRGB(i, j, newpixelvalue);
-                }
-                //bottom left corner
-                else if (i==0&&j==h-1)
-                {
-                    x[0] = img.getRGB(i, j);
-                    x[1] = img.getRGB(i, j-1);
-                    x[2] = img.getRGB(i+1, j-1);
-                    x[3] = img.getRGB(i+1, j);
-                    newpixelvalue = (x[0]+x[1]+x[2]+x[3])/4;
-                    nu.setRGB(i, j, newpixelvalue);
-                }
-                //bottom right corner
-                else if (i==w-1&&j==h-1)
-                {
-                    x[0] = img.getRGB(i, j);
-                    x[1] = img.getRGB(i-1, j-1);
-                    x[2] = img.getRGB(i, j-1);
-                    x[3] = img.getRGB(i-1, j);
-                    newpixelvalue = (x[0]+x[1]+x[2]+x[3])/4;
-                    nu.setRGB(i, j, newpixelvalue);
-                    
-                }
-                //left column
-                else if(i==0)
-                {
-                    x[0] = img.getRGB(i, j);
-                    x[1] = img.getRGB(i+1, j);
-                    x[2] = img.getRGB(i, j+1);
-                    x[3] = img.getRGB(i+1, j+1);
-                    newpixelvalue = (x[0]+x[1]+x[2]+x[3])/4;
-                    nu.setRGB(i, j, newpixelvalue);
-                    
-                }
-                //top row
-                else if(j==0)
-                {
-                    x[0] = img.getRGB(i, j);
-                    x[1] = img.getRGB(i-1, j);
-                    x[2] = img.getRGB(i-1, j+1);
-                    x[3] = img.getRGB(i, j+1);
-                    newpixelvalue = (x[0]+x[1]+x[2]+x[3])/4;
-                    nu.setRGB(i, j, newpixelvalue);
-                    
-                }
-                //bottom row
-                else if(j==h-1)
-                {
-                    x[0] = img.getRGB(i, j);
-                    x[1] = img.getRGB(i, j-1);
-                    x[2] = img.getRGB(i-1, j-1);
-                    x[3] = img.getRGB(i-1, j);
-                    newpixelvalue = (x[0]+x[1]+x[2]+x[3])/4;
-                    nu.setRGB(i, j, newpixelvalue);
-                }
-                //right column
-                else if(i==w-1)
-                {
-                    x[0] = img.getRGB(i, j);
-                    x[1] = img.getRGB(i-1, j);
-                    x[2] = img.getRGB(i, j+1);
-                    x[3] = img.getRGB(i-1, j+1);
-                    newpixelvalue = (x[0]+x[1]+x[2]+x[3])/4;
-                    nu.setRGB(i, j, newpixelvalue);
-                    
-                }
-                else
-                {
-                    p[0] = img.getRGB(i-1, j-1);
-                    p[1] = img.getRGB(i, j-1);
-                    p[2] = img.getRGB(i+1, j-1);
-                    p[3] = img.getRGB(i-1, j);
-                    p[4] = img.getRGB(i, j);
-                    p[5] = img.getRGB(i+1, j);
-                    p[6] = img.getRGB(i-1, j+1);
-                    p[7] = img.getRGB(i, j+1);
-                    p[8] = img.getRGB(i+1, j+1);
 
-
-                    newpixelvalue = (p[0]+p[1]+p[2]+p[3]+p[4]+p[5]+p[6]+p[7]+p[8])/9;
-                    nu.setRGB(i, j, newpixelvalue);
-                }
-            
-            
-            }
-        }
-        
-        
-        try{
-            String[] names = ImageIO.getWriterFormatNames();
-            f = new File("new.bmp");
-            
-//            for(String s: names)
-//            {
-//                System.out.print(s+ " ");
-//            }
-            boolean xd = ImageIO.write(nu, "bmp", f);
-            System.out.println("\n"+xd);
+             ImageIO.write(redrawn, "bmp", f);
+            //System.out.println("\n"+xd);
             
             
         }catch (Exception e)
         {
             System.out.println(e.getLocalizedMessage());
         }
+        
     }
+    
+    public void processFile()
+    {
+        for(int i =0; i<width; i++)
+            
+        {
+            
+            for(int j=0;j<height ;j++ )
+            {   
+                //top left corner
+                if(i==0 &&j==0)
+                { 
+                    smallSquare[0] = current.getRGB(i, j);
+                    smallSquare[1] = current.getRGB(i, j+1);
+                    smallSquare[2] = current.getRGB(i+1, j+1);
+                    smallSquare[3] = current.getRGB(i+1, j);
+                    newpixelvalue = this.getAverage(smallSquare);
+                    redrawn.setRGB(i, j, newpixelvalue);
+                    
+                }
+                //top right corner
+                else if(i==width-1&&j==0)
+                {
+                    smallSquare[0] = current.getRGB(i, j);
+                    smallSquare[1] = current.getRGB(i-1, j);
+                    smallSquare[2] = current.getRGB(i-1, j+1);
+                    smallSquare[3] = current.getRGB(i, j+1);
+                    newpixelvalue = this.getAverage(smallSquare);
+                    redrawn.setRGB(i, j, newpixelvalue);
+                }
+                //bottom left corner
+                else if (i==0&&j==height-1)
+                {
+                    smallSquare[0] = current.getRGB(i, j);
+                    smallSquare[1] = current.getRGB(i, j-1);
+                    smallSquare[2] = current.getRGB(i+1, j-1);
+                    smallSquare[3] = current.getRGB(i+1, j);
+                    newpixelvalue = this.getAverage(smallSquare);
+                    redrawn.setRGB(i, j, newpixelvalue);
+                }
+                //bottom right corner
+                else if (i==width-1&&j==height-1)
+                {
+                    smallSquare[0] = current.getRGB(i, j);
+                    smallSquare[1] = current.getRGB(i-1, j-1);
+                    smallSquare[2] = current.getRGB(i, j-1);
+                    smallSquare[3] = current.getRGB(i-1, j);
+                    newpixelvalue = this.getAverage(smallSquare);
+                    redrawn.setRGB(i, j, newpixelvalue);
+                    
+                }
+                //left column
+                else if(i==0)
+                {
+                    smallSquare[0] = current.getRGB(i, j);
+                    smallSquare[1] = current.getRGB(i+1, j);
+                    smallSquare[2] = current.getRGB(i, j+1);
+                    smallSquare[3] = current.getRGB(i+1, j+1);
+                    newpixelvalue = this.getAverage(smallSquare);
+                    redrawn.setRGB(i, j, newpixelvalue);
+                    
+                }
+                //top row
+                else if(j==0)
+                {
+                    smallSquare[0] = current.getRGB(i, j);
+                    smallSquare[1] = current.getRGB(i-1, j);
+                    smallSquare[2] = current.getRGB(i-1, j+1);
+                    smallSquare[3] = current.getRGB(i, j+1);
+                    newpixelvalue = this.getAverage(smallSquare);
+                    redrawn.setRGB(i, j, newpixelvalue);
+                    
+                }
+                //bottom row
+                else if(j==height-1)
+                {
+                    smallSquare[0] = current.getRGB(i, j);
+                    smallSquare[1] = current.getRGB(i, j-1);
+                    smallSquare[2] = current.getRGB(i-1, j-1);
+                    smallSquare[3] = current.getRGB(i-1, j);
+                    newpixelvalue = this.getAverage(smallSquare);
+                    redrawn.setRGB(i, j, newpixelvalue);
+                }
+                //right column
+                else if(i==width-1)
+                {
+                    smallSquare[0] = current.getRGB(i, j);
+                    smallSquare[1] = current.getRGB(i-1, j);
+                    smallSquare[2] = current.getRGB(i, j+1);
+                    smallSquare[3] = current.getRGB(i-1, j+1);
+                    newpixelvalue = this.getAverage(smallSquare);
+                    redrawn.setRGB(i, j, newpixelvalue);
+                    
+                }
+                else
+                {
+                    largeSquare[0] = current.getRGB(i-1, j-1);
+                    largeSquare[1] = current.getRGB(i, j-1);
+                    largeSquare[2] = current.getRGB(i+1, j-1);
+                    largeSquare[3] = current.getRGB(i-1, j);
+                    largeSquare[4] = current.getRGB(i, j);
+                    largeSquare[5] = current.getRGB(i+1, j);
+                    largeSquare[6] = current.getRGB(i-1, j+1);
+                    largeSquare[7] = current.getRGB(i, j+1);
+                    largeSquare[8] = current.getRGB(i+1, j+1);
+
+
+                    newpixelvalue = this.getAverage(largeSquare);
+                    redrawn.setRGB(i, j, newpixelvalue);
+                }
+            
+            
+            }
+        }
+        
+        saveFile();
+        
+    }
+    
+    public int getAverage(int[] array){
+        int total=0;
+        for(int n:array)
+            total = total+n;
+        
+        total = total/array.length;
+        
+        return total;
+    }
+    
+}
+
+
+
+public class ImageProcessingProject {
+    
+    public static void main (String[] args){
+        ImageProcess imageProcess = null;  
+        BufferedImage current = null;
+        try
+        {
+            current = ImageIO.read(new File("old.bmp"));
+            imageProcess = new ImageProcess(current);
+            
+        }catch (Exception e)
+        {
+            System.out.println(e.getLocalizedMessage());
+        }
+        
+        imageProcess.processFile();
+    }
+      
     
 }
