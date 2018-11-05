8e42970866ed74860929d2d71903210789af5b75
LovinK <lovinkuruvilla@gmail.com>
2018-11-05 10:37:11 -0600

    Fixed bug for multithread
    Fixed bottom rows and left columns that was not getting redrawn.
    
    cleaned up code.

--- src/ImageProcessingProject.java
+++ src/ImageProcessingProject.java
@@ -5,8 +5,11 @@
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import javax.imageio.ImageIO;
 
 class ImageProcess {
@@ -31,8 +34,8 @@ class ImageProcess {
             File f = new File("new.bmp");
             
 
-             ImageIO.write(redrawn, "bmp", f);
-            //System.out.println("\n"+xd);
+            ImageIO.write(redrawn, "bmp", f);
+            
             
             
         }catch (Exception e)
@@ -185,21 +188,22 @@ public class ImageProcessingProject {
         try
         {
             current = ImageIO.read(new File("old.bmp"));
-            //imageProcess = new ImageProcess(current);
+          //  imageProcess = new ImageProcess(current);
             
         }catch (Exception e)
         {
             System.out.println(e.getLocalizedMessage());
         }
-//        
-//        imageProcess.processFile();
-        ExecutorService executor = Executors.newCachedThreadPool();
+        
+      //  imageProcess.processFile();
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        
         int height = current.getHeight();
         
         int width = current.getWidth();
-       // BufferedImage redrawn = 
-        mThread.current = current;
-        mThread.redrawn = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
+       
+        myThread.current = current;
+        myThread.redrawn = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
         int division =8;
         
         int hfactor, wfactor;
@@ -209,46 +213,54 @@ public class ImageProcessingProject {
         
             wfactor = width/division;
         
-     
+            
         
-        int hstartIndex, hendIndex, wstartIndex, wendIndex;
+        int heightStartIndex, heightEndIndex, widthStartIndex, widthEndIndex;
         
+//        Runnable run = new mThread(0, width, 0, height);
+//                
+//                executor.execute(run);
+      
         
         for (int i =0; i<division; i++)
         {  
             
-            wstartIndex = i*wfactor;
-            wendIndex = wstartIndex + wfactor-1;
+            widthStartIndex = i*wfactor;
+            widthEndIndex = widthStartIndex + wfactor;
             
             if(i==division -1){
-                wstartIndex =i*wfactor;
-                wendIndex = width-1;
+                widthStartIndex =i*wfactor;
+                widthEndIndex = width;
             }
-//            
+           
             for(int j=0; j<division; j++)
             {
-                hstartIndex =j*hfactor;
-                hendIndex = hstartIndex + hfactor-1;
+                heightStartIndex =j*hfactor;
+                heightEndIndex = heightStartIndex + hfactor;
                 
                 if(j==division -1){
-                hstartIndex =j*hfactor;
-                hendIndex = height-1;
+                heightStartIndex =j*hfactor;
+                heightEndIndex = height;
                 }
                 
-                Runnable run = new mThread(wstartIndex, wendIndex, hstartIndex, hendIndex);
-                
+                Runnable run = new myThread(widthStartIndex, widthEndIndex, heightStartIndex, heightEndIndex);
                 executor.execute(run);
-                mThread.saveFile();
-                int x = 1+2;
+                
+                
+                
+                
+               
                 
             }
         }
-  
+        
         executor.shutdown();
-        while(!executor.isTerminated()){
-            //System.out.println("waiting..");
-        }
-        mThread.saveFile();
+        try{
+        executor.awaitTermination(20, TimeUnit.SECONDS);
+        }catch (Exception e)
+        {System.out.println(e.getLocalizedMessage());}
+        
+        myThread.saveFile();
         
         
         final long duration = System.currentTimeMillis() - startTime;
@@ -259,7 +271,7 @@ public class ImageProcessingProject {
     }
 }
     
-    class mThread implements Runnable{
+    class myThread implements Runnable{
     public static BufferedImage current,redrawn;
     public int[] largeSquare;
     public int[] smallSquare;
@@ -268,122 +280,12 @@ public class ImageProcessingProject {
     public int wstart, hstart;
     @Override
     public void run() {
-       // processFile();
-       for(int i =wstart; i<width; i++)
-            
-        {
-            
-            for(int j=hstart;j<height ;j++ )
-            {   
-                //top left corner
-                if(i==wstart &&j==hstart)
-                { 
-                    smallSquare[0] = current.getRGB(i, j);
-                    smallSquare[1] = current.getRGB(i, j+1);
-                    smallSquare[2] = current.getRGB(i+1, j+1);
-                    smallSquare[3] = current.getRGB(i+1, j);
-                    newpixelvalue = this.getAverage(smallSquare);
-                    redrawn.setRGB(i, j, newpixelvalue);
-                    
-                }
-                //top right corner
-                else if(i==width-1&&j==hstart)
-                {
-                    smallSquare[0] = current.getRGB(i, j);
-                    smallSquare[1] = current.getRGB(i-1, j);
-                    smallSquare[2] = current.getRGB(i-1, j+1);
-                    smallSquare[3] = current.getRGB(i, j+1);
-                    newpixelvalue = this.getAverage(smallSquare);
-                    redrawn.setRGB(i, j, newpixelvalue);
-                }
-                //bottom left corner
-                else if (i==wstart&&j==height-1)
-                {
-                    smallSquare[0] = current.getRGB(i, j);
-                    smallSquare[1] = current.getRGB(i, j-1);
-                    smallSquare[2] = current.getRGB(i+1, j-1);
-                    smallSquare[3] = current.getRGB(i+1, j);
-                    newpixelvalue = this.getAverage(smallSquare);
-                    redrawn.setRGB(i, j, newpixelvalue);
-                }
-                //bottom right corner
-                else if (i==width-1&&j==height-1)
-                {
-                    smallSquare[0] = current.getRGB(i, j);
-                    smallSquare[1] = current.getRGB(i-1, j-1);
-                    smallSquare[2] = current.getRGB(i, j-1);
-                    smallSquare[3] = current.getRGB(i-1, j);
-                    newpixelvalue = this.getAverage(smallSquare);
-                    redrawn.setRGB(i, j, newpixelvalue);
-                    
-                }
-                //left column
-                else if(i==wstart)
-                {
-                    smallSquare[0] = current.getRGB(i, j);
-                    smallSquare[1] = current.getRGB(i+1, j);
-                    smallSquare[2] = current.getRGB(i, j+1);
-                    smallSquare[3] = current.getRGB(i+1, j+1);
-                    newpixelvalue = this.getAverage(smallSquare);
-                    redrawn.setRGB(i, j, newpixelvalue);
-                    
-                }
-               // top row
-                else if(j==hstart)
-                {
-                    smallSquare[0] = current.getRGB(i, j);
-                    smallSquare[1] = current.getRGB(i-1, j);
-                    smallSquare[2] = current.getRGB(i-1, j+1);
-                    smallSquare[3] = current.getRGB(i, j+1);
-                    newpixelvalue = this.getAverage(smallSquare);
-                    redrawn.setRGB(i, j, newpixelvalue);
-                    
-                }
-                //bottom row
-                else if(j==height-1)
-                {
-                    smallSquare[0] = current.getRGB(i, j);
-                    smallSquare[1] = current.getRGB(i, j-1);
-                    smallSquare[2] = current.getRGB(i-1, j-1);
-                    smallSquare[3] = current.getRGB(i-1, j);
-                    newpixelvalue = this.getAverage(smallSquare);
-                    redrawn.setRGB(i, j, newpixelvalue);
-                }
-                //right column
-                else if(i==width-1)
-                {
-                    smallSquare[0] = current.getRGB(i, j);
-                    smallSquare[1] = current.getRGB(i-1, j);
-                    smallSquare[2] = current.getRGB(i, j+1);
-                    smallSquare[3] = current.getRGB(i-1, j+1);
-                    newpixelvalue = this.getAverage(smallSquare);
-                    redrawn.setRGB(i, j, newpixelvalue);
-                    
-                }
-                else
-                {
-                    largeSquare[0] = current.getRGB(i-1, j-1);
-                    largeSquare[1] = current.getRGB(i, j-1);
-                    largeSquare[2] = current.getRGB(i+1, j-1);
-                    largeSquare[3] = current.getRGB(i-1, j);
-                    largeSquare[4] = current.getRGB(i, j);
-                    largeSquare[5] = current.getRGB(i+1, j);
-                    largeSquare[6] = current.getRGB(i-1, j+1);
-                    largeSquare[7] = current.getRGB(i, j+1);
-                    largeSquare[8] = current.getRGB(i+1, j+1);
-
-
-                    newpixelvalue = this.getAverage(largeSquare);
-                    redrawn.setRGB(i, j, newpixelvalue);
-                }
-            
-            
-            }
-        }
+        processFile();
+       
     }
     
     
-    public mThread(int startwidth,int width, int startheight,int height){
+    public myThread(int startwidth,int width, int startheight,int height){
         
         this.width = width;
         this.height = height;
@@ -400,8 +302,8 @@ public class ImageProcessingProject {
             File f = new File("new.bmp");
             
 
-             ImageIO.write(redrawn, "bmp", f);
-            //System.out.println("\n"+xd);
+            ImageIO.write(redrawn, "bmp", f);
+           
             
             
         }catch (Exception e)
